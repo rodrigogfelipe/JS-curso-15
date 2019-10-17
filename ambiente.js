e3f89f5bd7ff054748d11ef09827182afc3f7f79
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-10-17 15:32:20 -0300

    Variaveis Compostas - 15

---
+++ ambiente.js
@@ -0,0 +1,14 @@
+/*
+let num= [5,8,9]
+num[3]= 6 -> Acrecentara mais uma posição no array inserido o elemento 6
+num.push(7) -> Acrecentara mais uma posição no array automatico inserido o elemento 7
+num.length -> LENGTH tamanho do array
+num.short() -> Orderna os elementos em ordens crecente
+*/
+
+let num=[5,8,2,9,3]
+//num.sort()
+//num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} quantidades`)
+console.log(`O primeiro valor da posição do vetor é ${num[0]}`)
